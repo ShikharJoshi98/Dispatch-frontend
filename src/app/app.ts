7101f4a26e693f6f6cb6f579a19c32dd3ddc08de
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-10 12:39:17 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('dispatch');
+  email = '';
+  password = '';
+  generate() {
+    alert(`email=>${this.email},password=>${this.password}`);
+  }
 }
